b0f96b9ebb3ecf21aac72fffbbeeaaa1ad8d9de4
Ferdinand Malcher <mail@ferdinand-malcher.de>
2018-05-17 11:14:46 +0200

    Reactive Forms und Validierung

--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { BooksRoutingModule } from './books-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
 import { BookRatingService } from './shared/book-rating.service';
+import { CreateBookComponent } from './create-book/create-book.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    CreateBookComponent
   ],
   providers: [
     BookRatingService
